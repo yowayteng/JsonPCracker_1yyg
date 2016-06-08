b16ab3eb876b7c131467c7dad7131f901fe2ea9a
yowayteng <gram2009@live.cn>
2016-06-08 20:09:19 +0800

    备份一下

--- src/JsonPCracker_1yyg/Web_LoadListener.java
+++ src/JsonPCracker_1yyg/Web_LoadListener.java
@@ -28,153 +28,34 @@ public class Web_LoadListener implements LoadListener {
 		BROWSER_COUNT = inBROWSER_COUNT;
 	}
 
-	boolean OverGrep;
-	public int BROWSER_ID;
-	public int BROWSER_COUNT;
 	@Override
 	public void onDocumentLoadedInFrame(FrameLoadEvent arg0) {
 		// TODO Auto-generated method stub
 
 	}
-
 	@Override
 	public void onDocumentLoadedInMainFrame(LoadEvent arg0) {
 		// TODO Auto-generated method stub
 	}
-
 	@Override
 	public void onFailLoadingFrame(FailLoadingEvent arg0) {
 		// TODO Auto-generated method stub
 	}
-	public boolean OpenWebAndClickShowList(FinishLoadingEvent arg0)
-	{
-		DOMDocument dom = arg0.getBrowser().getDocument();
-		DOMElement el =  dom.findElement(By.id("ul_menu"));
-		List<DOMElement> lis = el.findElements(By.tagName("li"));
-		for(DOMElement li : lis) 
-		{
-			if(li.getInnerHTML().contains("所有参与记录"))
-			{
-				li.click();		
-				try {
-					Thread.sleep(400);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				return true;
-			}
-		}
-		return false;
+	@Override
+	public void onProvisionalLoadingFrame(ProvisionalLoadingEvent arg0) {
+		// TODO Auto-generated method stub
+
 	}
-	public boolean CheckDataGetOK(FinishLoadingEvent arg0)
-	{
-		DOMDocument domNew = arg0.getBrowser().getDocument();
-		List<DOMElement> ulsNew =  domNew.findElements(By.tagName("ul"));
-		for (DOMElement ult : ulsNew)
-		{
-			if(ult.getInnerHTML().contains("查看云购码") == true)
-			{
-				return true;
-			}
-		}
-		return false;
+	@Override
+	public void onStartLoadingFrame(StartLoadingEvent arg0) {
+		// TODO Auto-generated method stub
 	}
-	public boolean LoggingData_toDB(
-			String showCode,
-			String hidGoodsID ,
-			String hidCodeID ,
-			String Title ,	
-			String buyid ,
-			String name ,
-			String ip ,
-			String time
-			)
-	{		
-		String rtunVal;
-		String url = "jdbc:ucanaccess://E:\\WorkSpace\\JAVA_1yyg.mdb" ;  
-		Properties properties = new Properties() ;  
-		properties.setProperty("charSet", "gbk") ;          //这里设置是为了防止查询的时候出现乱码  
-		Connection conn = null;  
-		try {
-			//Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
-			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		ResultSet rs = null;
-		java.sql.Statement statement;
-		String Sql = "";
-		try {
-			conn = DriverManager.getConnection(url);			
-			statement = conn.createStatement();	
-			{
-			Sql += "SELECT Count(*) AS RTab ";
-			Sql += "FROM MSysObjects ";
-			Sql += "WHERE (((MSysObjects.Name) Like 'YunBuyTable_"+ hidGoodsID + hidCodeID +"'));";
-			}
-			rs = statement.executeQuery(Sql);  
-			rs.next();
-			rtunVal = new String(rs.getString(1).getBytes(),"UTF-8");
-			
-			if(rtunVal.contains("0"))
-			{
-				Sql = "Create TABLE YunBuyTable_"+hidGoodsID+hidCodeID;
-				Sql += "(showCode TEXT(255) primary key,hidGoodsID_hidCodeID TEXT(255),Title TEXT(255),buyid TEXT(255),name TEXT(255),ip TEXT(255),buytime TEXT(255));";
-				statement.executeUpdate(Sql); 
-			}
-			else
-			{
-				Sql =  "SELECT ";
-				Sql += "FROM YunBuyTable_"+hidGoodsID+hidCodeID;
-				Sql += "WHERE showCode='"+showCode+"'";
-				rs = statement.executeQuery(Sql); 
-				if(rs.next() == false)
-				{
-					OverGrep = true;
-					return true;
-				}
-			}
-			{
-				Sql = "INSERT INTO YunBuyTable_"+hidGoodsID+hidCodeID;
-				Sql += "(showCode,hidGoodsID_hidCodeID,Title,buyid,name,ip,buytime) VALUES (";
-				Sql += 	showCode + ",";               //云购码
-				Sql += 	hidGoodsID + hidCodeID +",";  //购买码
-				Sql += 	Title +",";                   //标题
-				Sql += 	buyid +",";		              //云购号
-				Sql += 	name  +",";                   //用户名
-				Sql += 	ip    +",";                   //用户IP
-				Sql += 	time  +",";					  //购买时间
-				Sql += ");";
-				rs = statement.executeQuery(Sql); 
-			}
-			 
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}  
-		finally{
-			if(conn != null)
-			{
-				try {
-					conn.close();
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		}
-		return true;
-	}
+	/*
+	 * 废弃
 	public boolean NextPage(FinishLoadingEvent arg0)
 	{
 		DOMDocument domNew = arg0.getBrowser().getDocument();
-		DOMElement g_pagination =  domNew.findElement(By.className("g-pagination"));
-		
+		DOMElement g_pagination =  domNew.findElement(By.className("g-pagination"));		
 		
 		DOMElement current = g_pagination.findElement(By.className("current"));
 		DOMElement current_A = current.findElement(By.tagName("a"));
@@ -221,16 +102,190 @@ public class Web_LoadListener implements LoadListener {
 		}
 		return false;
 	}
+	*/
+	
+	public boolean OpenWebAndClickShowList(FinishLoadingEvent arg0)
+	{
+		DOMDocument dom = arg0.getBrowser().getDocument();
+		DOMElement el =  dom.findElement(By.id("ul_menu"));
+		List<DOMElement> lis = el.findElements(By.tagName("li"));
+		for(DOMElement li : lis) 
+		{
+			if(li.getInnerHTML().contains("所有参与记录"))
+			{
+				li.click();		
+				try {
+					Thread.sleep(400);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+	public boolean CheckDataGetOK(FinishLoadingEvent arg0)
+	{
+		DOMDocument domNew = arg0.getBrowser().getDocument();
+		DOMElement div_allrecord =  domNew.findElement(By.id("div_allrecord"));
+		if(div_allrecord.getInnerHTML().contains("查看云购码") == true)
+		{
+			return true;
+		}
+		return false;
+	}
+	public boolean LoggingData_toDB(
+			String showCode,
+			String hidGoodsID ,
+			String hidCodeID ,
+			String Title ,	
+			String buyid ,
+			String name ,
+			String ip ,
+			String time
+			)
+	{		
+		//String url = "jdbc:ucanaccess://E:\\WorkSpace\\JAVA_1yyg.mdb" ;  
+		String url = "jdbc:mysql://127.0.0.1:3306/db_1yyg";
+		String username = "5105243920";
+		String password = "spdh123";
+		Connection conn = null;  
+		try {
+			//Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		ResultSet rs = null;
+		java.sql.Statement statement;
+		String Sql = "";
+		try {
+			conn = DriverManager.getConnection(url,username,password);			
+			statement = conn.createStatement();		
+			//如果没有表建新表
+			{
+				Sql  = "create  table if not exists YunBuyTable_"+hidGoodsID+hidCodeID+"(";
+				Sql += "`showCode` int(11) NOT NULL,";
+				Sql += "`hidGoodsID` varchar(45) DEFAULT NULL,";
+				Sql += "`hidCodeID` varchar(45) DEFAULT NULL,";
+				Sql += "`Title` varchar(255) DEFAULT NULL,";
+				Sql += "`buyid` varchar(45) DEFAULT NULL,";
+				Sql += "`name` varchar(45) DEFAULT NULL,";
+				Sql += "`ip` varchar(45) DEFAULT NULL,";
+				Sql += "`buytime` varchar(45) DEFAULT NULL,";
+				Sql += "PRIMARY KEY (`showCode`)";
+				Sql += ") ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+				statement.executeUpdate(Sql); 
+			}
+			//查询数据是否重复
+			{
+				Sql =  "Select * FROM YunBuyTable_"+hidGoodsID+hidCodeID;
+				Sql += "WHERE showCode='" + showCode + "'";
+				rs = statement.executeQuery(Sql);
+				if(rs.next() == true)
+				{
+					OverGrep = true;
+					return true;
+				}
+			}
+			//插入数据
+			{
+				Sql = "INSERT INTO YunBuyTable_"+hidGoodsID+hidCodeID;
+				Sql += "(showCode,hidGoodsID,hidCodeID,Title,buyid,name,ip,buytime) VALUES (";
+				Sql += 	showCode + ",";               //云购码
+				Sql += 	hidGoodsID +",";              //购买码
+				Sql += 	hidCodeID +",";               //购买码
+				Sql += 	Title +",";                   //标题
+				Sql += 	buyid +",";		              //云购号
+				Sql += 	name  +",";                   //用户名
+				Sql += 	ip    +",";                   //用户IP
+				Sql += 	time  +",";					  //购买时间
+				Sql += ");";
+				statement.executeUpdate(Sql); 
+			}
+			 
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		} 
+		finally{
+			if(conn != null)
+			{
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 	public boolean NextPageByIndex(FinishLoadingEvent arg0)
 	{
+		int iNowIndex2 = 0;
 		DOMDocument domNew = arg0.getBrowser().getDocument();
 		DOMElement div_allrecord = domNew.findElement(By.id("div_allrecord"));
 		DOMElement div_g_pagination = div_allrecord.findElement(By.className("g-pagination"));
 		DOMElement span_current = div_g_pagination.findElement(By.className("current"));
+		DOMElement  total_em = div_g_pagination.findElement(By.tagName("em"));
+		DOMElement  input_box;
+		input_box = div_g_pagination.findElement(By.tagName("input"));
+		DOMElement  a_boutton;
+		a_boutton = div_g_pagination.findElement(By.id("btnGotoPage"));
 		DOMElement a_current = span_current.findElement(By.tagName("a"));
 		String  strNowIndex = a_current.getInnerText();
+		String  strTotalIndex = total_em.getInnerText();
 		int iNowIndex  = Integer.parseInt(strNowIndex);
-		
+		int iTotalIndex  = Integer.parseInt(strTotalIndex);
+		//如果第一次
+		if(Last_Index == 0)
+		{
+			iNowIndex = BROWSER_ID;
+		}
+		//不是第一次
+		else if(Last_Index == iNowIndex)
+		{
+			iNowIndex = BROWSER_ID + BROWSER_COUNT;
+		}
+		else if(Last_Index > iNowIndex)
+		{
+			iNowIndex = Last_Index;
+		}
+
+		//如果超出则不跳转
+		if(iNowIndex > iTotalIndex)
+		{
+			OverGrep = true;
+			return true;
+		}
+		do
+		{
+			input_box.setAttribute("value", ""+ iNowIndex);
+			a_boutton.click();
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			DOMDocument domNew2 = arg0.getBrowser().getDocument();
+			DOMElement div_allrecord2 = domNew2.findElement(By.id("div_allrecord"));
+			DOMElement div_g_pagination2 = div_allrecord2.findElement(By.className("g-pagination"));
+			DOMElement span_current2 = div_g_pagination2.findElement(By.className("current"));
+			DOMElement a_current2 = span_current2.findElement(By.tagName("a"));
+			String  strNowIndex2 = a_current2.getInnerText();
+			iNowIndex2 = Integer.parseInt(strNowIndex2);
+			input_box = div_g_pagination2.findElement(By.tagName("input"));
+			a_boutton = div_g_pagination2.findElement(By.id("btnGotoPage"));
+		}
+		while( iNowIndex != iNowIndex2 || CheckDataGetOK(arg0) == false);
+		Last_Index = iNowIndex;	
 		return true;		
 	}
 	public boolean LoggingData(FinishLoadingEvent arg0)
@@ -296,9 +351,7 @@ public class Web_LoadListener implements LoadListener {
 						for(DOMElement code_lisItem : code_lis)
 						{
 							try{
-								FileWriter fw = new FileWriter("E:\\WorkSpace\\JsonPCracker_1yyg_ForJava\\log.txt",true);												
-								showCode = code_lisItem.getInnerText();
-								/*
+								showCode = code_lisItem.getInnerText();							
 								LoggingData_toDB(
 										 showCode,
 										 hidGoodsID, 
@@ -307,8 +360,10 @@ public class Web_LoadListener implements LoadListener {
 										 buyid ,
 										 name ,
 										 ip ,
-										 time);
-								*/
+										 time);	
+								/*
+								FileWriter fw = new FileWriter("E:\\WorkSpace\\JsonPCracker_1yyg_ForJava\\log.txt",true);												
+					
 								fw.write(""+showCode.replace("\n", "")+" ,");
 								fw.write(""+hidGoodsID.replace("\n", "")+" ,");
 								fw.write(""+hidCodeID.replace("\n", "")+" ,");
@@ -319,6 +374,7 @@ public class Web_LoadListener implements LoadListener {
 								fw.write(""+time.replace("\n", "")+"");
 								fw.write("\n");
 								fw.close();
+								*/
 							}
 							catch(Exception e)
 							{
@@ -330,21 +386,23 @@ public class Web_LoadListener implements LoadListener {
 		return true;
 	}
 		
+	boolean OverGrep;
+	public int BROWSER_ID;
+	public int BROWSER_COUNT;
+	public int Last_Index;
+	
 	@Override
 	public void onFinishLoadingFrame(FinishLoadingEvent arg0)
 	{
 		OverGrep = false;
+		Last_Index = 0;
 		while(true)
 		{
 			//点击界面清单
 			if(OpenWebAndClickShowList(arg0) == false)
 			{
 				//错误处理
-			}
-			//检查是否有数据刷出来
-			if(CheckDataGetOK(arg0) == true)
-			{
-				break;
+				
 			}
 		}
 		while(true)
@@ -384,7 +442,7 @@ public class Web_LoadListener implements LoadListener {
 			{
 				
 			}
-			if(NextPage(arg0) == false)
+			if(NextPageByIndex(arg0) == false)
 			{
 				break;
 			}
@@ -417,16 +475,5 @@ public class Web_LoadListener implements LoadListener {
 			
 		}
 	}
-
-	@Override
-	public void onProvisionalLoadingFrame(ProvisionalLoadingEvent arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void onStartLoadingFrame(StartLoadingEvent arg0) {
-		// TODO Auto-generated method stub
-	}
-
+	
 }
